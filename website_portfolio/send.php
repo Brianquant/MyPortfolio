7c938b199be0f40010262ff35db6bc20fdf851ab
reifferscheid.brianquant <reifferscheid.brianquant@gmail.com>
2021-01-11 21:48:04 +0100

    changed layout

--- website_portfolio/send.php
+++ website_portfolio/send.php
@@ -7,8 +7,17 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
     
     $mailTo = "reifferscheid.brian@outlook.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You received an e-amil";
+    $subject = "Email from my website";
+    $body = "You received an e-amil";
+
+    $body .= "\r\n Name: " . $name;
+	$body .= "\r\n Email: " . $mail;
+	$body .= "\r\n Number: " . $number;
+	$body .= "\r\n Message: " . $message;
+
+    $headers 		= 'From: PHP Email Tutorial <support@myWebsite.com>' . "\r\n" .
+    				'Reply-To: support@myWebsite.com' . "\r\n" .
+    				'X-Mailer: PHP/' . phpversion();
 
 
     mail($mailTo, $txt, $headers);
