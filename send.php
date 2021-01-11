d57bbc36e73fa1bed63e6a1f01777d74a696bae1
reifferscheid.brianquant <reifferscheid.brianquant@gmail.com>
2021-01-11 19:05:38 +0100

    updated form

--- send.php
+++ send.php
@@ -6,9 +6,13 @@ if (isset($_POST['submit'])) {
     $number = $_POST['number'];
     $message = $_POST['message'];
     
-    $mailTo = "reifferscheid@brianstemplats.site";
+    $mailTo = "reifferscheid.brian@outlook.com";
     $headers = "From: ".$mailFrom;
-    $txt = "You received an e-amil from ".$name.".\n\n".$message;
+    $txt = "You received an e-amil";
+
+    $body .= "\r\n Name: " . $name;
+	$body .= "\r\n Email: " . $mail;
+	$body .= "\r\n Message: " . $message;
 
     mail($mailTo, $txt, $headers);
     header("Location: index.php?mailsend");
