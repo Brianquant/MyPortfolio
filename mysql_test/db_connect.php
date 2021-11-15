f09bdc9a35d611bb4fe48366df9e9e72d311830f
Brianquant <reifferscheid.brian@outlook.com>
2021-11-15 19:41:45 +0100

    test5

--- mysql_test/db_connect.php
+++ mysql_test/db_connect.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html><head><meta charset="utf-8"></head></html>
 <?php 
 
-$con = mysqli_connect("brianstemplats.site", "u439520744_Brian", 'P9"JG9SAT<u+', "u439520744_firma");
+$con = mysqli_connect("brianstemplats.site/mysql_test/db_connect.php", "u439520744_Brian", 'P9"JG9SAT<u+', "u439520744_firma");
 $res = mysqli_query($con, "SELECT * FROM personen");
 
 while($dsatz = mysqli_fetch_assoc($res)) {
