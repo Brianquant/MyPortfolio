38fb024bcd0bc50deeb2485ccefb19721037ab34
Brianquant <reifferscheid.brian@outlook.com>
2021-11-15 19:31:30 +0100

    test

--- mysql_test/db_connect.inc.php
+++ mysql_test/db_connect.inc.php
@@ -1,5 +1,7 @@
 <?php 
 
-$con = mysqli_connect("brianstemplats.site", "Brian", 'P9"JG9SAT<u+', "u439520744_firma");
+$con = mysqli_connect("https://brianstemplats.site/mysql_test/db_connect.php", "Brian", 'P9"JG9SAT<u+', "u439520744_firma");
+
+var_dump($con);
 
 ?>
