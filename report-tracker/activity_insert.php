ff7d3bbcbe6f519a023c9c59d2484bc2afba2c22
Brianquant <reifferscheid.brianquant@gmail.com>
2022-05-01 22:46:00 +0200

    added new project

---
+++ report-tracker/activity_insert.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html><html><head><meta charset="utf-8"></head></html>
+<?php 
+
+$conn = mysqli_connect("localhost", "u439520744_Brian", "1234Test#1234", "u439520744_firma");
+var_dump($con);
+
+$activity = $_POST["activity"];
+$lernfeld = $_POST["lernfeld"];
+
+// Create connection
+// Study $conn = mysqli_connect("localhost", "root", "", "work_log_report_db");
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+function table_exists($table) {
+    global $conn;
+    $table = $conn->real_escape_string($table);
+    $sql = "SHOW TABLES LIKE '$table' ";
+    $result = $conn->query($sql);
+    $num = 0;
+    $num = mysqli_num_rows($result);
+    if($num > 0) {
+        return true;
+    }
+    return false;
+  }
+  
+  // usage
+  $my_table_name = "reg_date";
+  if(table_exists($table = $my_table_name)) {
+
+    $sql = "INSERT INTO `BS1styear` (activity, lernfeld)
+   VALUES ('$activity','$lernfeld')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Insert Data created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }
+  } else {;
+
+    $sql = "CREATE TABLE `BS` (
+        `id` INT AUTO_INCREMENT NOT NULL,
+        `activity` VARCHAR(30) NOT NULL,
+        `lernfeld` VARCHAR(30) NOT NULL,
+        `reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        primary key (id)
+        )";     
+        if (mysqli_query($conn, $sql)) {
+            echo "Table created successfully";
+          } else {
+            echo "Error creating table: " . mysqli_error($conn);
+          }
+
+    $sql = "INSERT INTO `BSstyear` (activity, lernfeld)
+    VALUES ('$activity','$lernfeld')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Insert Data created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }
+  } 
+
+
+mysqli_close($conn);
+
+
+?>
+</body></html>
---
+++ report-tracker/activity_tracker_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity Tracker</title>
+</head>
+<body>
+    <form action="./activity_insert.php" method="post">
+        <p>Lernfeld <input type="text" name="lernfeld"></p>
+        <p>Activity <input type="text" name="activity"></p>
+        <!-- <p>Datum <input type="text" name="datum"></p> -->
+        <input type="submit" value="Track activity">
+    </form>
+</body>
+</html>
