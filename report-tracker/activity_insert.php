2c55e53bea17394d9f5080609a3dc813b98fa775
Brianquant <reifferscheid.brianquant@gmail.com>
2022-05-02 11:14:09 +0200

    changed varchar count

--- report-tracker/activity_insert.php
+++ report-tracker/activity_insert.php
@@ -42,7 +42,7 @@ function table_exists($table) {
 
     $sql = "CREATE TABLE `BS` (
         `id` INT AUTO_INCREMENT NOT NULL,
-        `activity` VARCHAR(30) NOT NULL,
+        `activity` VARCHAR(126) NOT NULL,
         `lernfeld` VARCHAR(30) NOT NULL,
         `reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         primary key (id)
