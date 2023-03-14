5d5f171defde1a905d7dcb636dca8bc2e24aa774
Brianquant <reifferscheid.brianquant@gmail.com>
2023-03-14 12:33:38 +0100

    corrected css path

--- report-tracker-generator/tracker/activity_tracker_form.php
+++ report-tracker-generator/tracker/activity_tracker_form.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Activity Tracker</title>
-    <link rel="stylesheet" href="../css/normalize.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="/report-tracker-generator/css/style.css">
+    <link rel="stylesheet" href="/report-tracker-generator/css/normalize.css">
 </head>
 <body>
 
